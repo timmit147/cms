d0f602944c83d20cdda101e9c37fc0725aa254e1
timmit147 <timmeeuwsen@hotmail.nl>
2023-07-28 16:37:44 +0200

    blocks insert working

---
+++ database.js
@@ -0,0 +1,38 @@
+{
+  "pages": {
+    "page1": {
+      "blocks": [
+        {
+          "type": "block1",
+          "title": "Title of Block 1 - Item 1",
+          "content": "Content of Block 1 - Item 1",
+          "link": "https://example.com/item1",
+          "link_text": "Link Text for Block 1 - Item 1"
+        },
+        {
+          "type": "block1",
+          "title": "Title of Block 1 - Item 2",
+          "content": "Content of Block 1 - Item 2",
+          "link": "https://example.com/item2",
+          "link_text": "Link Text for Block 1 - Item 2"
+        },
+        {
+          "type": "block2",
+          "title": "Title of Block 2",
+          "content": "Content of Block 2",
+          "link": "https://example.com/block2",
+          "link_text": "Link Text for Block 2"
+        },
+        {
+          "type": "block3",
+          "title": "Title of Block 3",
+          "content": "Content of Block 3",
+          "link": "https://example.com/block3",
+          "link_text": "Link Text for Block 3"
+        }
+      ]
+    },
+    "page2": {},
+    "page3": {}
+  }
+}
