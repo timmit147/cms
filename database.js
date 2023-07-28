9e88532cd12d78a5383645bd01d57d837a1e51f3
timmit147 <timmeeuwsen@hotmail.nl>
2023-07-28 20:03:37 +0000

    Update title as specified in the payload

--- database.js
+++ database.js
@@ -4,7 +4,7 @@
       "blocks": [
         {
           "type": "block1",
-          "title": "test",
+          "title": "testing123",
           "content": "Content of Block 1 - Item 1",
           "link": "https://example.com/item1"
         },
