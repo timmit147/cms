069c1c6ddc4e715bada8a5da2b32c98d14fe18d7
timmit147 <timmeeuwsen@hotmail.nl>
2023-07-30 17:00:51 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,34 +1,74 @@
-name: Add New Block
-
-on:
-  repository_dispatch:
-    types: add_block
-
-jobs:
-  add_block:
-    runs-on: ubuntu-latest
-
-    steps:
-    - name: Checkout repository
-      uses: actions/checkout@v2
-
-    - name: Display received data
-      run: |
-        # The block name will be passed as an input to the workflow
-        echo "Block Name: ${{ github.event.client_payload.blockName }}"
-        
-        # Find the template file based on the block name and read its content
-        templateFile=$(find . -type f -path "./blocks/${{ github.event.client_payload.blockName }}/templates.js")
-        
-        # If the template file is found, read its content and store it in a variable
-        if [ -f "$templateFile" ]; then
-          templateContent=$(cat "$templateFile")
-          echo "$templateContent" > templateData.js
-          echo "Template content:"
-          echo "$templateContent"
-        else
-          echo "Template file not found: $templateFile"
-          exit 1
-        fi
-
-    # Add more steps to perform the desired actions with the template data.
+{
+  "pages": {
+    "page1": {
+      "blocks": [
+        {
+          "type": "block1",
+          "title": "does this code work....",
+          "content": "Content of Block 1 - Item 1",
+          "link": "https://example.com/item1"
+        },
+        {
+          "type": "block1",
+          "title": "zandkasteel",
+          "content": "Content of Block 1 - Item 2",
+          "link": "pizza",
+          "link_text": "Link Text for Block 1 - Item 2"
+        },
+        {
+          "type": "block2",
+          "title": "pindakaaspot",
+          "content": "hallo this is a test",
+          "link": "https://example.com/block2",
+          "link_text": "Link Text for Block 2",
+          "hash": "8b203baad0691282bcdafd9cac654dae3c950de6"
+        },
+        {
+          "type": "block3",
+          "title": "Title of Block 3",
+          "content": "Content of Block 3",
+          "link": "https://example.com/block3",
+          "link_text": "Link Text for Block 3"
+        },
+        {
+          "type": "block4",
+          "title": "apple",
+          "content": "apples en peren"
+        }
+      ]
+    },
+    "page2": {
+      "blocks": [
+        {
+          "type": "test",
+          "title": "does this code work...."
+        }
+      ]
+    },
+    "page3": {
+      "blocks": [
+        {
+          "type": "Hallo",
+          "title": "hallo"
+        }
+      ]
+    }
+  },
+  "hash": "868216a6b2ef31b6a370ebc562d4bb3c299ce4c98b07b75054f82ad548403153"
+}
+{
+  "type": "block4",
+  "title": "apple",
+  "content": "apples en peren",
+  "pages": {
+    "page1": {
+      "blocks": [
+        {
+          "type": "block4",
+          "title": "apple",
+          "content": "apples en peren"
+        }
+      ]
+    }
+  }
+}
